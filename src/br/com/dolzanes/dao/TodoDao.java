816530b48cda5cc37de90be23b8f746bb9d22368
fdolzanes1 <fdolzanes1@gmail.com>
2020-06-24 16:49:51 -0400

    add todo dao

---
+++ src/br/com/dolzanes/dao/TodoDao.java
@@ -0,0 +1,19 @@
+package br.com.dolzanes.dao;
+
+import java.sql.SQLException;
+
+import br.com.dolzanes.model.Todo;
+
+public interface TodoDao {
+	
+	 void insertTodo(Todo todo) throws SQLException;
+
+	 Todo selectTodo(long todoId);
+
+	 List<Todo> selectAllTodos();
+
+	 boolean deleteTodo(int id) throws SQLException;
+
+	 boolean updateTodo(Todo todo) throws SQLException;
+	 
+}
